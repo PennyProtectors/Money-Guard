9be6f228eafdd256760d658afcb04c1ac69cb75f
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-07 14:05:24 +0300

    Add ButtonAddTransactions component with styles

---
+++ src/components/ButtonAddTransactions/ButtonAddTransactions.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import css from "./ButtonAddTransactions.module.css";
+const ButtonAddTransactions = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className={css.buttonAddTransaction}>
+      <svg
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M10 0V20" stroke="white" stroke-width="2" />
+        <path d="M0 10L20 10" stroke="white" stroke-width="2" />
+      </svg>
+    </button>
+  );
+};
+
+export default ButtonAddTransactions;
