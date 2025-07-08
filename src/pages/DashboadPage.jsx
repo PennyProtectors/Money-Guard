dd2d430b2e1b0e622baa74cf15d92fd0763a5331
Muhammet Burak Altun <altunburak88@gmail.com>
2025-07-08 18:52:24 +0300

    balance and currency comp add and fix

--- src/pages/DashboadPage.jsx
+++ src/pages/DashboadPage.jsx
@@ -1,20 +1,20 @@
-import Header from "../components/Header"
-import React from 'react'
-import css from "./DashboardPage.module.css"
+import React from 'react';
+import Header from '../components/Header';
+import Balance from '../components/Balance/Balance';
+import Currency from '../components/Currency/Currency';
+import css from './DashboardPage.module.css';
 
-function DashboadPage() {
+function DashboardPage() {
   return (
     <div className={css.dashboard}>
-        <Header/>
-        <div>
-            <h6>YOUR BALANCE</h6>
-            <P>{purchase}</P>
-        </div>
-        
+      <Header />
 
-       
+      <main className={css.content}>
+        <Balance />
+        <Currency />
+      </main>
     </div>
-  )
+  );
 }
 
-export default DashboadPage
+export default DashboardPage;
