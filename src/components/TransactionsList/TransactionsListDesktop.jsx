e98a9dc68bde7465cd6d13eed3a6b3f84d00f82a
Mürsel ŞEN <murselsen_35@hotmail.com>
2025-07-11 11:49:22 +0300

    Refactor TransactionsListDesktop to include <thead> and <tbody> for better table structure

--- src/components/TransactionsList/TransactionsListDesktop.jsx
+++ src/components/TransactionsList/TransactionsListDesktop.jsx
@@ -7,21 +7,24 @@ const TransactionsListDesktop = () => {
 
   return (
     <table className={css.transactionsList_Table}>
-      <tr
-        className={[
-          css.transactionsList_Table_Row,
-          css.transactionsList_Table_Header,
-        ].join(" ")}
-      >
-        <th className={css.column}>Date</th>
-        <th className={css.column}>Type</th>
-        <th className={css.column}>Category</th>
-        <th className={css.column}>Comment</th>
-        <th colSpan="1" className={css.column}>
-          Sum
-        </th>
-        <th></th>
-      </tr>
+      <thead>
+        <tr
+          className={[
+            css.transactionsList_Table_Row,
+            css.transactionsList_Table_Header,
+          ].join(" ")}
+        >
+          <th className={css.column}>Date</th>
+          <th className={css.column}>Type</th>
+          <th className={css.column}>Category</th>
+          <th className={css.column}>Comment</th>
+          <th colSpan="1" className={css.column}>
+            Sum
+          </th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
       {data && data.length > 0
         ? data.map((transaction) => (
             <TransactionsListDesktop_Item
@@ -29,7 +32,9 @@ const TransactionsListDesktop = () => {
               transaction={transaction}
             />
           ))
-        : null}
+          : null}
+        
+        </tbody>
     </table>
   );
 };
