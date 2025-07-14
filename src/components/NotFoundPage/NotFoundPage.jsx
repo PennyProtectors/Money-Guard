8a2c880fee4718e43a5c05782b001b3c3e1238b6
Özge SEVİM <ozgsvm@gmail.com>
2025-07-15 01:49:22 +0300

    fixed auth operations file& notfounda page added

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -1,19 +1,22 @@
-import { Link } from 'react-router-dom';
-import css from './NotFoundPage.css';
+import { Link } from "react-router-dom";
+import css from "./NotFoundPage.module.css";
 
 const NotFoundPage = () => {
-    return (
-        <div className={css.container}>
-            <div className={css.content}>
-                <h1 className={css.title}>404</h1>
-                <h2 className={css.subtitle}>Page Was Not Found</h2>
-                <p className={css.description}>This content is currently unavailable. Please check the URL or return to our homepage.</p>
-                <Link to="/" className={css.button}>
-                    Back to Home
-                </Link>
-            </div>
-        </div>
-    );
+  return (
+    <div className={css.container}>
+      <div className={css.content}>
+        <h1 className={css.title}>404</h1>
+        <h2 className={css.subtitle}>Page Was Not Found</h2>
+        <p className={css.description}>
+          This content is currently unavailable. Please check the URL or return
+          to our homepage.
+        </p>
+        <Link to="/" className={css.button}>
+          Back to Home
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFoundPage;
